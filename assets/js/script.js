7a2dfc1f9776be0b0c4495fd0056b07988b5fd64
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-05-11 12:41:48 -0500

    added function for city input and 5 day weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,31 @@ var weatherContainerEl = document.querySelector("#current-weather");
 var citySearchInputEl = document.querySelector("#searched-city");
 var forecastHeader = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
+
+var formSumbitHandler = function (event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if (city) {
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({ city });
+        cityInputEl.value = "";
+    } else {
+        alert("Please Enter a Valid City!");
+    }
+    saveSearch();
+}
+
+var saveSearch = function () {
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+var getCityWeather = function (city) {
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=cb888a3ff2e15203b988ff3728172064`
+    fetch(apiURL)
+        .then(function (response) {
+            response.json().then(function (data) {
+                displayWeather(data, city);
+            });
+        });
+};
