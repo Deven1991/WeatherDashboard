ac432a3c4ebaed35679f15be749f965e21469b0c
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-05-12 12:15:58 -0500

    updated JavaScript file

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ Additionally the UV Index changes color to indicate whether the conditions are g
 
 ## Used in Webpage
 
-The HTML, CSS, and JavaScript files used to create this webpage utilize sources from open weather APIs, specifically current weather conditions, UV index, and 5 day forecast. Additionally bootstrap is used and moment.JS APIs in order to pull in current time.
+The HTML, CSS, and JavaScript files used to create this webpage utilize sources from open weather APIs, specifically current weather conditions, UV index, and 5 day forecast. Additionally bootstrap is used and moment.JS APIs in order to pull in current time. The [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) was used to retrieve weather data for cities.
+
+Inputs for searched cities are saved in local storage.
 
 ## Screenshot
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -119,3 +119,48 @@ var get5Day = function (city) {
             });
         });
 };
+
+var display5Day = function (weather) {
+    forecastContainerEl.textContent = ""
+    forecastHeader.textContent = "5 Day Forecast Below";
+
+    var forecast = weather.list;
+    for (var i = 5; i < forecast.length; i = i + 8) {
+        var dailyForecast = forecast[i];
+
+        var forecastEl = document.createElement("div");
+        forecastEl.classList = "card bg-primary text-light m-2";
+
+        var forecastDate = document.createElement("h5")
+        forecastDate.textContent = moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+        forecastDate.classList = "card-header text-center"
+        forecastEl.appendChild(forecastDate);
+
+        //Creates Weather Icon
+        var weatherIcon = document.createElement("img")
+        weatherIcon.classList = "card-body text-center";
+        weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);
+        forecastEl.appendChild(weatherIcon);
+
+        //Creates Temperature in 5 Day Forecast
+        var forecastTempEl = document.createElement("span");
+        forecastTempEl.classList = "card-body text-center";
+        forecastTempEl.textContent = "Temp: " + dailyForecast.main.temp + " °F";
+        forecastEl.appendChild(forecastTempEl);
+
+        //Creates Wind in 5 Day Forecast
+        var forecastWindEl = document.createElement("span");
+        forecastWindEl.classList = "card-body text-center";
+        forecastWindEl.textContent = "Wind: " + dailyForecast.wind.speed + " MPH";
+        forecastEl.appendChild(forecastWindEl);
+
+        //Creates Humidity in 5 Day Forecast
+        var forecastHumEl = document.createElement("span");
+        forecastHumEl.classList = "card-body text-center";
+        forecastHumEl.textContent = "Humidity: " + dailyForecast.main.humidity + "  %";
+        forecastEl.appendChild(forecastHumEl);
+        forecastContainerEl.appendChild(forecastEl);
+    }
+}
+
+cityFormEl.addEventListener("submit", formSumbitHandler);
